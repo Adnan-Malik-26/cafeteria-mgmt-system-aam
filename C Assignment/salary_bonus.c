95b608dfaac4f706723636a584c3cf57b368bb7e
mr-insxne <110969113+mr-insxne@users.noreply.github.com>
2023-01-09 23:28:40 +0530

    My Changes

--- C Assignment/salary_bonus.c
+++ C Assignment/salary_bonus.c
@@ -4,7 +4,7 @@ int main()
     char sex;
     int salary,bonus,salary_after_bonus;
     
-    printf("Please Enter Your Gender(M for male and F for Female): ");
+    printf("Please Enter Your Gender(M for male and F for Female): h     ");
     scanf("%c",&sex);
     switch (sex)
     {
