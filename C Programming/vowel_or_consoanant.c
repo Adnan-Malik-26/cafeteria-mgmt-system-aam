42fd972c738d921d9d570b9f227211bf1e47dbad
adnan-malik <eddymalikadnan@gmail.com>
2023-02-10 11:29:29 +0530

    My Changes

--- C Programming/vowel_or_consoanant.c
+++ C Programming/vowel_or_consoanant.c
@@ -4,6 +4,7 @@ void main()
     char alpha;
     printf("Please Enter the Character: ");
     scanf("%c",&alpha);
+    
     if (alpha=='a'||alpha=='e'||alpha=='i'||alpha=='o'||alpha=='u')
     {
         printf("The Alphabet is a Vowel.");
