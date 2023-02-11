f9a40f8bde5bc232f563ca9b2a7f35ba631f571d
adnan-malik <eddymalikadnan@gmail.com>
2023-02-11 14:28:15 +0530

    My Changes

---
+++ C Programming/array_largest.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int num[5],i;
+    num[1]=12;
+    num[2]=1;
+    num[3]=3;
+    num[4]=6;
+    num[5]=9;
+    printf("The Array is given below: \n %d\t%d\t%d\t%d\t%d\t",num[1],num[2],num[3],num[4],num[5]);
+    for (i = 0; i < 5; i++)
+    {
+        
+        
+    }
+    
+}
